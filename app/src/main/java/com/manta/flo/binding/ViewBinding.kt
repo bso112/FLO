9951f6ce2cfd9e6b82653594f097278722203a59
byeon <bso11246@gmail.com>
2021-02-04 15:09:08 +0900

    Add progress bar

--- app/src/main/java/com/manta/flo/binding/ViewBinding.kt
+++ app/src/main/java/com/manta/flo/binding/ViewBinding.kt
@@ -1,10 +1,16 @@
 package com.manta.flo.binding
 
+import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 object ViewBinding {
     @JvmStatic
@@ -16,11 +22,25 @@ object ViewBinding {
     }
 
     @JvmStatic
-        @BindingAdapter("image_uri")
-    fun getImageFromUri(view: View, uri: String?) {
+    @BindingAdapter("image_uri", "progressBar")
+    fun getImageFromUri(view: View, uri: String?, progressBar: ProgressBar) {
         if (view !is ImageView) return;
         uri?.let {
-            Glide.with(view).load(uri).into(view);
+            Glide.with(view)
+                .load(uri)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+                }
+                )
+                .into(view);
         }
     }
 
